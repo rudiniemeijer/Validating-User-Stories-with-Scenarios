5c2bd016e18713a0fec1ff293c115a58696e9720
Rudi Niemeijer <rudiniemeijer@gmail.com>
2019-02-24 09:26:52 +0100

    Removed a typo

--- splinter_tryout.py
+++ splinter_tryout.py
@@ -1,21 +1,23 @@
-from splinter import Browser
+from splinter.browser import Browser
+# from hamcrest import * # Can be used as an alternative
 import logging
-logging.basicConfig(level=logging.INFO)
 
-browser = Browser('chrome')
+logging.basicConfig(level=logging.INFO) # logs to the screen
+
+browser = Browser('chrome') #also: 'firefox'
 
 browser.visit("https://duckduckgo.com")
+
 browser.fill('q', 'raspberry pi retro website')
 button = browser.find_by_id('search_button_homepage')
 button.click()
 
 if browser.is_text_present('Retro-Lab'):
-  logging.info("Found it!")
+  logging.debug("Found it!")
 else:
-  logging.warning("Did not find it!")
+  logging.debug("Did not find it!")
 
-# Alternative 
-# import hamcrest
-# assert_true(browser.html, contains_string('Retro-Lab'))
+# Alternative
+# assert_that(browser.html, contains_string('Retro-Lab'))
 
 browser.quit()
