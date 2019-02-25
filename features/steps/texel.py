66f0d7d66f395514a345723282ce14963331f1fd
Rudi Niemeijer <rudiniemeijer@gmail.com>
2019-02-25 06:43:48 +0100

    Some changes may not work..

--- features/steps/texel.py
+++ features/steps/texel.py
@@ -1,53 +1,45 @@
 from behave import *
-import hamcrest
-
-@given(u'I have navigated to the Texel.net website')
-def step_impl(context):
-  context.browser.visit(context.basicurl)
-
+from hamcrest import *
 
 @given(u'the home page is in front of me')
 def step_impl(context):
   context.browser.visit(context.basicurl)
 
-
 @given(u'I see a button with a country flag')
 def step_impl(context):
-  context.execute_steps("I have navigated to the Texel.net website")
-
+  context.execute_steps("Given the home page is in front of me")
   
 @when(u'I select my preferred language flag')
 def step_impl(context):
-    context.preferred_language = "en"    context.browser.visit(context.basicurl + "/" + context.userdata["preferred_language"])
-
+    context.browser.visit(context.basicurl + "/" + context.userdata["preferred_language"])
 
 @then(u'the page is translated into my preferred language')
 def step_impl(context):
     if context.userdata["preferred_language"] == "en":
-        assert_that(context.browser.html, contains_text("WHAT'S ON"))
-
+        assert_that(context.browser.html, contains_string("What's on"))
 
 @given(u'I have selected the English language')
 def step_impl(context):
-    context.browser.visit(context.basicurl + "/en/")
-
+    context.browser.visit(context.basicurl + "en/")
 
 @then(u'I see the following subjects')
 def step_impl(context):
-    for subject in context.table:
-        assert_that(context.browser.html, contains_text(subject))
-
+    for row in context.table:
+        context.loggit.info("Checking subject <%s> on this page", row["subject"])
+        # do some tests here
 
 @given(u'I am on the {subject} page')
 def step_impl(context, subject):
-    raise NotImplementedError(u'STEP: Given I am on the WHAT\'S ON page')
-
+    context.loggit.info("Fetching basic url: <%s>, subject is <%s>", context.basicurl + "en/", subject)
+    context.browser.visit(context.basicurl + "en/")
+    context.browser.click_link_by_text(subject)
+    context.loggit.info("Clicked it")
 
 @when(u'I scan the page')
 def step_impl(context):
-    context.current_page = context.browser.html
-
+    pass
 
 @then(u'I find information about {nugget}')
 def step_impl(context, nugget):
-    assert_that(context.current_page, contains_text(nugget))
+    #assert_that(context.current_page, contains_string(nugget))
+    assert_that(context.browser.is_text_present(nugget))
