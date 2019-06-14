795a615ca584a95ed2ba9517e814bc4b3792b8fd
Rudi Niemeijer <rudiniemeijer@gmail.com>
2019-06-14 13:30:49 +0200

    Update logging_with_a_single_handler.py

--- logging_with_a_single_handler.py
+++ logging_with_a_single_handler.py
@@ -3,13 +3,15 @@ import logging
 logformat = "%(levelname)s:%(filename)s:%(lineno)d:%(asctime)s:%(message)s"
 logfile = "./example.log"
 loglevel = logging.INFO
+#loglevel = logging.DEBUG
 
 my_formatter = logging.Formatter(logformat)
 my_handler = logging.FileHandler(logfile)
 my_handler.setFormatter(my_formatter)
-my_handler.setLevel(loglevel)
 
 my_logger = logging.getLogger('root_name')
 my_logger.addHandler(my_handler)
+my_logger.setLevel(loglevel)
 
-my_logger.info("We're on!")
+my_logger.info("We're on info!")
+my_logger.debug("And on debug!")
